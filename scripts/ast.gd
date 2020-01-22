7529e8507355b4b3c1d5c48ed370cbfbe29fb0f5
Cristian Morataya <cris.morataya@gmail.com>
2020-01-22 08:11:43 -0600

    Fix Ast movement, make sprite child of area2d

--- scripts/ast.gd
+++ scripts/ast.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 export (int) var speed = 400
-const radian = 10
 
 func _ready():
 	OS.set_window_size(Vector2(320, 240))
@@ -11,10 +10,8 @@ func _process(delta):
 	var velocity = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-		rotate(radian)
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-		rotate(-radian)
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
